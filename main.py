e9003ad70718df8624df992b29e4a856a9a9f821
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-11-18 09:56:59 +1300

    Add get_service_points_der function and update service point usage retrieval

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from datetime import datetime
 
 customer_id = CUSTOMER_ID
 utility_id = UTILITY_ID
+
 securityContext = "Voltello/CUSTOMERS/Individual/"
-
 base_url = "https://acapi.vecddevau1.village.energy/xv1"
-
 service_points_endpoint = "/get/customer/energy/electricity/servicepoints/"
+
 headers = {
     "apiKey": API_TOKEN,
     "Accept": "*/*",
@@ -23,7 +23,7 @@ def get_service_points_list(customer_id):
     url = f"{base_url}{service_points_endpoint}{securityContext}{customer_id}"
     params = {
         "customerId": customer_id
-   }
+    }
     response = requests.get(url, headers=headers, params=params)
     
     #print (url)
@@ -40,7 +40,7 @@ def get_service_points_list(customer_id):
 def get_service_points_detail(servicepointID):
     # Endpoint 
     url = f"{base_url}{service_points_endpoint}{servicePointID}/{securityContext}{customer_id}"
-    print (url)
+    #print (url)
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
@@ -48,12 +48,24 @@ def get_service_points_detail(servicepointID):
     else:
         print(f"Error: {response.json()['Message']}")
         return None
+    
+def get_service_points_der(servicepointID):
+
+    url = f"{base_url}{service_points_endpoint}{servicepointID}/der/{securityContext}{customer_id}/"
+     #print (url)
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.json()['Message']}")
+        return None    
 
 
-#  get usage
+
 def get_service_points_usage(servicepointID, from_date, to_date, returnAllTelemetry, returnData):
 
-    url = f"{base_url}/get/customer/energy/electricity/servicepoints/{servicepointID}/usage/{securityContext}{customer_id}/"
+    url = f"{base_url}{service_points_endpoint}{servicepointID}/usage/{securityContext}{customer_id}/"
     params = {
         "fromDate": from_date,
         "toDate": to_date,
@@ -69,7 +81,6 @@ def get_service_points_usage(servicepointID, from_date, to_date, returnAllTeleme
         print(f"Error: {response.json()['Message']}")
         return None
     
-# Get the service points list
 
 print ("-------------------------------------------------------------------------------------------------")
 print ("------------------------------    Service Points List   -----------------------------------------")
@@ -87,6 +98,13 @@ print ("------------------------------------------------------------------------
 service_points_list = get_service_points_detail(servicePointID)
 print(json.dumps(service_points_list, indent=4))
 
+print ("-------------------------------------------------------------------------------------------------")
+print ("------------------------------    Service Points DER -----------------------------------------")
+print ("-------------------------------------------------------------------------------------------------")
+
+service_points_der = get_service_points_der(servicePointID)
+print(json.dumps(service_points_list, indent=4))
+
 
 print ("-------------------------------------------------------------------------------------------------")
 print ("------------------------------     Service Usage Today  -----------------------------------------")
@@ -94,7 +112,7 @@ print ("------------------------------------------------------------------------
 from_date = datetime.today().strftime('%Y-%m-%d')
 to_date = datetime.today().strftime('%Y-%m-%d')
 returnAllTelemetry = "true"
-returnData = "dailyTotal" # aligned5MinRead, aligned15MinRead, aligned30MinRead, alignedHourlyRead, dailyTotal
+returnData = "dailyTotal" # alignedminRead, aligned15minRead, aligned30minRead, alignedHourlyRead, dailyTotal
 usage_list = get_service_points_usage(servicePointID, from_date, to_date, returnAllTelemetry, returnData)
 print(json.dumps(usage_list, indent=4))
 
@@ -105,7 +123,7 @@ print ("------------------------------------------------------------------------
 from_date = datetime.today().strftime('%Y-%m-%d')
 to_date = datetime.today().strftime('%Y-%m-%d')
 returnAllTelemetry = "true"
-returnData = "aligned5MinRead" # aligned5MinRead, aligned15MinRead, aligned30MinRead, alignedHourlyRead, dailyTotal
+returnData = "aligned5minRead" 
 usage_list = get_service_points_usage(servicePointID, from_date, to_date, returnAllTelemetry, returnData)
 print(json.dumps(usage_list, indent=4))
 
@@ -115,7 +133,7 @@ print ("------------------------------------------------------------------------
 from_date = datetime.today().strftime('%Y-%m-%d')
 to_date = datetime.today().strftime('%Y-%m-%d')
 returnAllTelemetry = "true"
-returnData = "aligned15MinRead" # aligned5minRead, aligned15MinRead, aligned30MinRead, alignedHourlyRead, dailyTotal
+returnData = "aligned15minRead"
 usage_list = get_service_points_usage(servicePointID, from_date, to_date, returnAllTelemetry, returnData)
 print(json.dumps(usage_list, indent=4))
 
