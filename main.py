469ce8c1978dc629c5a1d0c21cc2c3918221f28d
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-11-18 10:27:19 +1300

    Import functions and constants from utils and defs modules in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
-import utils.py
-import defs.py
+from utils.py import *
+from defs.py import *
 from requests_toolbelt.utils import dump
 from secrets import API_TOKEN, CUSTOMER_ID, UTILITY_ID
 from datetime import datetime
