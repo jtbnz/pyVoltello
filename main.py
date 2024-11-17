1c4480b505203d0a5a49d42b18d619546e0b42fd
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-11-18 10:15:00 +1300

    Fix URL formatting in get_live_data function and update request logging

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def get_service_points_usage(servicepointID, from_date, to_date, returnAllTeleme
     
 def get_live_data(servicepointID):
     #apiPath="/get/customer/energy/electricity/servicepoints/${servicePointId}/usage/${customerSecurityContext}?action=getPowerForServicePoint&returnFormat=VE"
-    url = f"{base_url}{service_points_endpoint}{servicepointID}/usage/{securityContext}{customer_id}/"
+    url = f"{base_url}{service_points_endpoint}{servicepointID}/usage/{securityContext}{customer_id}"
     print (url)
     params = {
         "action": "getPowerForServicePoint",
@@ -84,7 +84,7 @@ def get_live_data(servicepointID):
     
 
     response = requests.get(url, headers=headers, params=params)
-    request_data = dump.dump_all(response.request)
+    request_data = dump.dump(response.request)
     print(request_data.decode('utf-8')) 
         
     if response.status_code == 200:
